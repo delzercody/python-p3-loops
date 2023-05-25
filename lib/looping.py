86c7405ccc87f898d2ff96319672a2c1abf76411
Cody Delzer <delzercody@gmail.com>
2023-05-25 13:44:30 -0400

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    counter = 1
+    while counter < 101:
+        if (counter % 3 == 0 and counter % 5 == 0):
+            print("FizzBuzz")
+        elif counter % 3 == 0:
+            print("Fizz")
+        elif counter % 5 == 0:
+            print("Buzz")
+        else:
+            print(counter)
+        counter += 1
+    
